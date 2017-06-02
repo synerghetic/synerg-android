0f418c1a28d933ca4ca98aae71508a995ad34c34
Aude <aude.ficheux@hetic.net>
2017-06-02 17:41:20 +0200

    Display one Instagram Img

--- App/app/src/main/java/net/synerghetic/synerg/fragments/Tab2Activite.java
+++ App/app/src/main/java/net/synerghetic/synerg/fragments/Tab2Activite.java
@@ -1,5 +1,8 @@
 package net.synerghetic.synerg.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
@@ -7,12 +10,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import net.synerghetic.synerg.R;
 import net.synerghetic.synerg.model.Instagram;
 import net.synerghetic.synerg.model.StandardResolution;
+import net.synerghetic.synerg.network.ImageLoadTask;
 import net.synerghetic.synerg.network.InstagramService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -25,7 +34,7 @@ public class Tab2Activite extends Fragment {
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab2activite, container, false);
+        final View rootView = inflater.inflate(R.layout.tab2activite, container, false);
 
         InstagramService.getInstagram(new InstagramService.InstagramListener() {
             @Override
@@ -33,6 +42,10 @@ public class Tab2Activite extends Fragment {
                 for (Instagram instagram : instagrams){
                     String imageUrl = instagram.getImages().getStandard_resolution().getUrl();
                     System.out.println(imageUrl);
+
+                    ImageView img = (ImageView) rootView.findViewById(R.id.instaView1);
+                    new ImageLoadTask(imageUrl, img).execute();
+                    //img.setImageBitmap(getBitmapFromURL(imageUrl));
                 }
             }
 
@@ -41,9 +54,6 @@ public class Tab2Activite extends Fragment {
 
             }
         });
-
-
-        
 
         return rootView;
     }
